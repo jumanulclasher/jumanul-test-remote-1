f5757c1799d1ca8f4cd217f26f2935c8c708e835
Jumanul Qalby <jumanulclasher@gmail.com>
2020-04-21 12:04:37 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>customizing app</title>
+  </head>
+  <body>
+    <h3>coba coba remote</h3>
+  </body>
+</html>
